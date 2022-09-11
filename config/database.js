dd8fdb44a76472990cc217965f31e60db097ce4f
datispham <datistpham@gmail.com>
2022-09-11 13:17:17 +0700

    commit message

--- config/database.js
+++ config/database.js
@@ -1,12 +1,12 @@
 module.exports = {
-	'url': 'mongodb://127.0.0.1:27017',
-	'options': {
-		'dbName': 'CLUB3333', // red
-		//'dbName': 'GAME', // red
-		//'dbName': 'admin', // red
-                //'dbName': 'vn11022021', // red
-		'useNewUrlParser': true,
-		'useUnifiedTopology': true,
-		//'autoIndex':       false,
-	},
+  url: "mongodb+srv://giang:giangvippro@cluster0.pffyt.mongodb.net/?retryWrites=true&w=majority",
+  options: {
+    dbName: "CLUB3333", // red
+    //'dbName': 'GAME', // red
+    //'dbName': 'admin', // red
+    //'dbName': 'vn11022021', // red
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    //'autoIndex':       false,
+  },
 };
